976a5bfb44992924fd61be07e57686da4dfdbd47
Ahmad <ahmadfaraz629@gmail.com>
2020-09-18 18:00:58 +0500

    :hammer: user selector code refactored

--- src/store/selectors/user.ts
+++ src/store/selectors/user.ts
@@ -4,11 +4,7 @@ import { IState, IUser, ISummary, Style } from 'types';
 export const getUsers = (state: IState) => state.user.users;
 
 export const getSortedUsers = createSelector([getUsers], users => {
-  return users.sort((a: IUser, b: IUser) => {
-    let dateA: any = new Date(a.created_at);
-    let dateB: any = new Date(b.created_at);
-    return dateA - dateB;
-  });
+  return users.sort((a: IUser, b: IUser) => new Date(a.created_at).valueOf() - new Date(b.created_at).valueOf());
 });
 
 export const getSummary = createSelector([getSortedUsers], (users: IUser[]) => {
@@ -16,16 +12,17 @@ export const getSummary = createSelector([getSortedUsers], (users: IUser[]) => {
     total_members: 0,
     published_campaigns_count: 0,
     invited_users_count: 0,
-    last_created: null
+    last_created: users.length > 0 ? new Date(users[users.length - 1].created_at) : null
   };
 
+  // calculate the total_members , invited_users_count and published_campaigns_count in one iteration
   return users.reduce((summary: ISummary, user: IUser) => {
     if (user.style === Style.member) summary.total_members++;
+
     if (user.stats.invited_users_count) summary.invited_users_count += user.stats.invited_users_count;
 
     if (user.stats.published_campaigns_count) summary.published_campaigns_count += user.stats.published_campaigns_count;
 
-    summary.last_created = new Date(user.created_at);
     return summary;
   }, initialSummary);
 });
